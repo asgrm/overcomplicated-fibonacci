12809237242920462489055a1a5c0a2d8f736f36
asgrm <andriigromakov@gmail.com>
2025-01-30 08:25:07 +0000

    main: version bump

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Fib Calculator version 2</p>
+          <p>Fib Calculator version 3</p>
           <a
             className="App-link"
             href="https://reactjs.org"
